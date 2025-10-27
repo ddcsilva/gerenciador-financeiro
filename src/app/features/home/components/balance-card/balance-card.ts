8b7198b16e3ede8af579ff6cefa9766190a55e43
Danilo Silva <danilo.silva@msn.com>
2025-10-26 21:35:39 -0300

    feat: melhorando tipagem de balance-card

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,6 +1,9 @@
 import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+type CardType = 'income' | 'outcome' | 'balance';
+type ValueCssClasses = 'income' | 'outcome';
+
 @Component({
   selector: 'app-balance-card',
   imports: [MatCardModule],
@@ -8,11 +11,11 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './balance-card.scss',
 })
 export class BalanceCard {
-  type = input.required<'income' | 'outcome' | 'balance'>();
+  type = input.required<CardType>();
   label = input.required<string>();
   value = input.required<number>();
 
-  cssClass = computed(() => {
+  cssClass = computed<ValueCssClasses>(() => {
     if (this.type() === 'income') {
       return 'income';
     }
