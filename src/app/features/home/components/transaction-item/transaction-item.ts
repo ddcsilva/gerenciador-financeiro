a4fc5e0f2541bcb0e5806ebfa1b240cb1bbbcc1b
Danilo Silva <danilo.silva@msn.com>
2025-10-27 22:29:49 -0300

    feat: criando a interface Transaction

--- src/app/features/home/components/transaction-item/transaction-item.ts
+++ src/app/features/home/components/transaction-item/transaction-item.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Transaction } from '../../../../shared/transaction/interfaces/transaction';
 
 @Component({
   selector: 'app-transaction-item',
@@ -8,4 +9,6 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './transaction-item.html',
   styleUrl: './transaction-item.scss',
 })
-export class TransactionItem {}
+export class TransactionItem {
+  transaction = input.required<Transaction>();
+}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
-import { TransactionItem } from "./components/transaction-item/transaction-item";
+import { TransactionItem } from './components/transaction-item/transaction-item';
+import { Transaction } from '../../shared/transaction/interfaces/transaction';
+import { TransactionType } from '../../shared/transaction/enums/transaction-type';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +11,9 @@ import { TransactionItem } from "./components/transaction-item/transaction-item"
   styleUrl: './home.scss',
 })
 export class Home {
-  transactions = signal([
-    { value: 100, type: 'income' },
-    { value: 50, type: 'income' },
-    { value: 150, type: 'outcome' },
+  transactions = signal<Transaction[]>([
+    { title: 'Salário', value: 100, type: TransactionType.INCOME },
+    { title: 'VA', value: 50, type: TransactionType.INCOME },
+    { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME },
   ]);
 }
